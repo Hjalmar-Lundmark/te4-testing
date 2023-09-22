dae1e13ee4d573a16d661e687fd80f426e371efb
Hjalmar-Lundmark <lundmarkhjalmar@gmail.com>
2023-09-22 09:57:35 +0200

    Small fixes, changed linebreaks in story.json

--- src/testGson.java
+++ src/testGson.java
@@ -1,7 +1,6 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
-
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
@@ -9,48 +8,44 @@ import com.google.gson.JsonObject;
 
 public class testGson {
     public static void main(String[] args) {
-        String storyFile = "src/story.json";
         Gson gson = new Gson();
         JsonObject[] arrObj;
         try {
-            arrObj = gson.fromJson(new FileReader(storyFile), JsonObject[].class);
+            arrObj = gson.fromJson(new FileReader("src/story.json"), JsonObject[].class);
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
 
-        Scanner q = new Scanner(System.in);
-        int qAns;
+        Scanner question = new Scanner(System.in);
+        int ans;
         int place = 0;
 
         // story starts
         while (true) {
-            JsonElement id = arrObj[place].get("id");
+            //JsonElement id = arrObj[place].get("id");
             JsonElement text = arrObj[place].get("text");
             JsonArray options = arrObj[place].get("options").getAsJsonArray();
 
-            System.out.println(text.toString()); // \n doesn't work
+            System.out.println(text.getAsString()); // \n doesn't work
 
             if (options.isEmpty()) {
                 break;
             }
+
             for (int i = 0; i < 2; i++) {
                 JsonObject option = options.get(i).getAsJsonObject();
                 JsonElement optionText = option.get("msg");
 
-                System.out.println(i+1 + ". " + optionText);
+                System.out.println(i+1 + ". " + optionText.getAsString());
             }
 
             //scanner listen
             System.out.println("Write the corresponding number below then press enter.");
-            qAns = q.nextInt();
-            System.out.println(qAns);
+            ans = question.nextInt();
 
-            JsonObject option = options.get(qAns-1).getAsJsonObject();
+            JsonObject option = options.get(ans-1).getAsJsonObject();
             JsonElement optionNextId = option.get("nextId");
             place = optionNextId.getAsInt();
         }
-
-        System.out.println("story completed");
-
     }
 }
